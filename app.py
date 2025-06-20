4f7d84debdeb8f0784809c4e9312a60ca30f6d82
jsr-xyx <Jesse1889420666@gmail.com>
2025-06-20 04:18:52 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 import streamlit as st
 import about, projects, contact, tracker
 
-st.set_page_config(page_title="Jesse Xue | Portfolio", layout="wide")
+st.set_page_config(page_title="Yunxuan(Jesse) Xue | Portfolio", layout="wide")
 
 st.sidebar.title("📌 Navigate")
 page = st.sidebar.radio("Go to:", ["🏠 Home", "📂 Projects", "👤 About", "📫 Contact", "🥏 Ultimate Tracker"])
 
 if page == "🏠 Home":
-    st.title("Jesse Xue")
-    st.subheader("Economics & Statistics Student")
+    st.title("Yunxuan(Jesse) Xue")
+    st.subheader("Economics Student")
     st.write("I use Python to turn real-world data into useful insight—on and off the field.")
     st.markdown("[📂 View Projects](#projects)")
     st.markdown("[📫 Contact Me](#contact)")
